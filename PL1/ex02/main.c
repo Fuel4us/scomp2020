fe9b2e79ad51da75fcd9d4d6c6c50d110381e546
Gonçalo <goncalo7bfc@gmail.com>
2020-02-20 17:56:06 +0000

    Ex02 edited :wrench:

--- PL1/ex02/main.c
+++ PL1/ex02/main.c
@@ -8,14 +8,18 @@ int main(void) {
 
   pid_t filho1 = fork();
 
+  wait(NULL);
+
   if (filho1 > 0){
     printf("the..\n");
     pid_t filho2 = fork();
+    wait(NULL);
     if (filho2 == 0) {
       printf("filho2\n");
     } else {
       printf("father\n");
       pid_t filho3 = fork();
+      wait(NULL);
       if(filho3 == 0) {
         printf("filho3\n");
       }
