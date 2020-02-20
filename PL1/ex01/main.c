59eb20fa5a912101cf9b4af852a117c8f2cf4f37
Gonçalo <goncalo7bfc@gmail.com>
2020-02-20 16:33:43 +0000

    Ex01 :white_check_mark:

---
+++ PL1/ex01/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main(void) {
+  int x = 1;
+  pid_t p = fork();
+
+  if(p == 0) {
+    x = x+1;
+    printf("1. x = %d\n", x);
+  } else {
+    x = x-1;
+    printf("2. x = %d\n", x);
+  }
+  printf("3. %d; x = %d\n", p, x);
+}
